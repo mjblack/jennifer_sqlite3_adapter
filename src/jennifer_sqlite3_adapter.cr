3613e9b6c8816397de813ac0a18c85dcac32801e
Matthew J. Black <mjblack@gmail.com>
2025-12-07 16:57:35 -0500

    Bumped version to 0.4.2

--- src/jennifer_sqlite3_adapter.cr
+++ src/jennifer_sqlite3_adapter.cr
@@ -9,7 +9,7 @@ require "./sqlite3/functions"
 module Jennifer
   module SQLite3
     # Library version.
-    VERSION = "0.4.1"
+    VERSION = "0.4.2"
 
     class Adapter < Adapter::Base
       include ::Jennifer::Adapter::RequestMethods
